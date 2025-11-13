b3552dd42d1edeb6a62e4ea4447220f1f95f9b86
dharmeesh-adeshwar <dharmeesh@adeshwarmarketing.com>
2025-11-13 14:49:52 +0530

    Trying to deploy on vercel

--- api/index.js
+++ api/index.js
@@ -1,13 +1,4 @@
-// Import from the built server file instead of source
-
-// The build process resolves the virtual:react-router/server-build import
-
 import handler from '../dist/server/index.js';
-
-
-
 export default {
-
   fetch: handler.fetch,
-
 };
